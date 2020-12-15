41d9270146126336397053eb608128c180d420db
waka-syu <hamutailang059@gmail.com>
2020-12-15 11:41:23 +0900

    Usersテーブル作成

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :last_kana, presence: true
+  validates :first_kana, presence: true
+  validates :birth_date, presence: true
+end
