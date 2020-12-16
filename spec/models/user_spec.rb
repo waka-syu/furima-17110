eaf7029d1df107ec49ea126f258c5cce71e58fa1
waka-syu <hamutailang059@gmail.com>
2020-12-16 16:00:19 +0900

    ユーザー管理機能の修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -64,6 +64,13 @@ RSpec.describe User, type: :model do
         expect(@user.errors.full_messages).to include("Password には英字と数字の両方を含めて設定してください")
       end
 
+      it "passwordが半角英数字混合でないと登録できない" do
+        @user.password = "aaaaaa"
+        @user.password_confirmation = "aaaaaa"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password には英字と数字の両方を含めて設定してください")
+      end
+
       it "passwordとpassword_confirmationが不一致では登録できないこと" do
         @user.password = "abc123"
         @user.password_confirmation = "aaa111"
