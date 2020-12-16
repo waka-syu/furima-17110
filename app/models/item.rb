213e3d32c48c6f184cac1b9e0b1cc898a39483fe
waka-syu <hamutailang059@gmail.com>
2020-12-16 18:28:56 +0900

    ActiveStorage導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,16 @@
 class Item < ApplicationRecord
+  belongs_to :user
+  has_one_attached :image
+
+  with_options presence: true do
+    validates :name
+    validates :text
+    validates :state_id
+    validates :category_id
+    validates :fee_id
+    validates :prefecture_id
+    validates :delivery_date_id
+    validates :value
+  end
+  
 end
