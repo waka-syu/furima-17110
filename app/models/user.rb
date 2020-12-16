4be3b9eb76291eee9a553721e32009d77c5937fd
waka-syu <hamutailang059@gmail.com>
2020-12-16 17:19:29 +0900

    バリデーション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
+  with_options presence: true do
+    validates :nickname
+    validates :birth_date
+  end
   with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: '全角文字を使用してください' } do
     validates :last_name
     validates :first_name
@@ -11,7 +14,6 @@ class User < ApplicationRecord
     validates :last_kana
     validates :first_kana
   end
-  validates :birth_date, presence: true
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'には半角の英数字を含めて設定してください'
 end
