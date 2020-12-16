7fbcddc94907e1b19359245323764f288135b94c
waka-syu <hamutailang059@gmail.com>
2020-12-16 19:43:02 +0900

    ActiveHash導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,16 +1,25 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :state
+  belongs_to :fee
+  belongs_to :delivery_date
+  belongs_to :prefecture
+
   belongs_to :user
   has_one_attached :image
 
   with_options presence: true do
     validates :name
     validates :text
+    validates :value
+  end
+  
+  with_options presence: true, numericality: { other_then: 1 } do
     validates :state_id
     validates :category_id
     validates :fee_id
     validates :prefecture_id
     validates :delivery_date_id
-    validates :value
   end
-  
 end
