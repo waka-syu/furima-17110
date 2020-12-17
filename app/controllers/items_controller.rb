58aa0d98fccf83e982f4810822b1e41f55cbe37d
waka-syu <hamutailang059@gmail.com>
2020-12-17 14:21:33 +0900

    出品商品のデータ保存

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,5 +7,19 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new
   end
-  
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :name, :text, :category_id, :state_id, :fee_id, :prefecture_id, :delivery_date_id, :value).merge(user_id: current_user.id)
+  end
 end
