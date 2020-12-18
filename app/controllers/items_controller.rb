1c31e2b3a64ed2cb7d940cdfb180184de5b5d4b3
waka-syu <hamutailang059@gmail.com>
2020-12-18 18:12:22 +0900

    編集ページ表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item, only: [:show, :edit]
 
   def index
     @items = Item.includes(:user).order("created_at DESC")
@@ -19,13 +20,16 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
   end
 
   def update
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 
   private
