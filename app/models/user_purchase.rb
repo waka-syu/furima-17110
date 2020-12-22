c0ad13e55d7fcaa41a6ef8de61a29da12a773c5b
waka-syu <hamutailang059@gmail.com>
2020-12-22 12:25:33 +0900

    購入フォームの作成

--- app/models/user_purchase.rb
+++ app/models/user_purchase.rb
@@ -1,6 +1,6 @@
 class UserPurchase
   include ActiveModel::Model
-  attr_assessor :postal_code, :prefecture_id, :city, :address_line, :building, :phone_num
+  attr_accessor :postal_code, :prefecture_id, :city, :address_line, :building, :phone_num
 
   with_options presence: true do
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
