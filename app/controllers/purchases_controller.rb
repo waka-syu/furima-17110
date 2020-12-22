93093382ff907eeec04257787ffa2a0820cffd19
waka-syu <hamutailang059@gmail.com>
2020-12-22 11:28:18 +0900

    モデルやコントローラーの作成

---
+++ app/controllers/purchases_controller.rb
@@ -0,0 +1,21 @@
+class PurchasesController < ApplicationController
+  def index
+    @user_purchase = UserPurchase.new
+  end
+
+  def create
+    @user_purchase = UserPurchase.new(purchase_params)
+    if @user_purchase.valid?
+      @user_purchase.save
+      redirect_to root_path
+    else
+      render action: :index
+    end
+  end
+
+  private
+
+  def purchase_params
+    params.require(:user_purchase).permit(:postal_code, :prefecture_id, :city, :address_line, :building, :phone_num)
+  end
+end
