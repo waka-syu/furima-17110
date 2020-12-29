97cbbb8195d1d8c022e5cf318ef4c3387712d0ce
waka-syu <hamutailang059@gmail.com>
2020-12-29 18:49:18 +0900

    マイページ修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,6 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @items = @user.items.order("created_at DESC")
     @purchases = Purchase.includes(:item)
-    @profile = Profile.find(@user.id)
+    @profile = Profile.find_by(user_id: params[:id])
   end
 end
