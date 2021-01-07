6e97617e6646f6744916ada6eb54f09a8b7095ae
waka-syu <hamutailang059@gmail.com>
2021-01-07 15:09:57 +0900

    javascriptのイベント発火ページ指定

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,10 +1,12 @@
-window.addEventListener('load', () => {
-  const priceInput = document.getElementById("item-price");
-  priceInput.addEventListener("input", () => {
-    const inputValue = priceInput.value;
-    const addTaxPrice = document.getElementById("add-tax-price");
-    addTaxPrice.innerHTML = Math.floor(inputValue * 0.1);
-    const profit = document.getElementById("profit");
-    profit.innerHTML = Math.floor(inputValue - addTaxPrice.innerHTML);
+if (document.URL.match( /new/ )) {
+  window.addEventListener('load', () => {
+    const priceInput = document.getElementById("item-price");
+    priceInput.addEventListener("input", () => {
+      const inputValue = priceInput.value;
+      const addTaxPrice = document.getElementById("add-tax-price");
+      addTaxPrice.innerHTML = Math.floor(inputValue * 0.1);
+      const profit = document.getElementById("profit");
+      profit.innerHTML = Math.floor(inputValue - addTaxPrice.innerHTML);
+    });
   });
-});
+}
